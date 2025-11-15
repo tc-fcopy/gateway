84fc90e2f5dcc0b78f7c57066fbf929f69e6588b
tc-fcopy <tc_fcopy@163.com>
2025-11-15 16:23:58 +0800

    11/15 update http service

--- controller/services.go
+++ controller/services.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"errors"
 	"fmt"
 	"gateway/dao"
 	"gateway/dto"
@@ -8,6 +9,7 @@ import (
 	"gateway/middleware"
 	"gateway/public"
 	"github.com/gin-gonic/gin"
+	"strings"
 )
 
 type ServiceController struct {
@@ -16,7 +18,13 @@ type ServiceController struct {
 func ServiceRegister(group *gin.RouterGroup) {
 	serviceController := &ServiceController{}
 	group.GET("/service_list", serviceController.ServiceList)
+	group.GET("/service_delete", serviceController.ServiceDelete)
+	group.GET("/service_detail", serviceController.ServiceDetail)
 	// group.POST("/change_pwd", adminController.AdminChangePwd)
+
+	group.POST("/service_add_http", serviceController.ServiceAddHTTP)
+	group.POST("/service_update_http", serviceController.ServiceUpdateHTTP)
+
 }
 
 func (service *ServiceController) ServiceList(c *gin.Context) {
@@ -126,4 +134,239 @@ func (service *ServiceController) ServiceDetail(c *gin.Context) {
 		return
 	}
 	middleware.ResponseSuccess(c, serviceDetail)
+}
+
+func (service *ServiceController) ServiceDelete(c *gin.Context) {
+	params := &dto.ServiceDeleteInput{}
+	if err := params.BindValidParam(c); err != nil {
+		middleware.ResponseError(c, 2000, err)
+		return
+	}
+
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+
+	//读取基本信息
+	serviceInfo := &dao.ServiceInfo{ID: params.ID}
+	serviceInfo, err = serviceInfo.Find(c, tx, serviceInfo)
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	serviceInfo.IsDelete = 1
+	if err := serviceInfo.Save(c, tx); err != nil {
+		middleware.ResponseError(c, 2003, err)
+		return
+	}
+	middleware.ResponseSuccess(c, "")
+}
+
+func (service *ServiceController) ServiceAddHTTP(c *gin.Context) {
+	params := &dto.ServiceAddHTTPInput{}
+	if err := params.BindValidParam(c); err != nil {
+		middleware.ResponseError(c, 2000, err)
+		return
+	}
+
+	if len(strings.Split(params.IpList, ",")) != len(strings.Split(params.WeightList, ",")) {
+		middleware.ResponseError(c, 2004, errors.New("IP列表与权重列表数量不一致"))
+		return
+	}
+
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	tx = tx.Begin()
+	serviceInfo := &dao.ServiceInfo{ServiceName: params.ServiceName}
+	info, err := serviceInfo.Find(c, tx, serviceInfo)
+	if err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2002, err) // 数据库真的出错了（比如表不存在）
+		return
+	}
+	// 关键判断：只有 ID > 0，才说明是从数据库查到的真实数据
+	if info != nil && info.ID > 0 {
+		tx.Rollback()
+		middleware.ResponseError(c, 2002, fmt.Errorf("服务名称 [%s] 已存在", params.ServiceName))
+		return
+	}
+
+	httpUrl := &dao.HttpRule{RuleType: params.RuleType, Rule: params.Rule}
+	httpRuleInfo, err := httpUrl.Find(c, tx, httpUrl)
+	if err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2003, err)
+		return
+	}
+	if httpRuleInfo != nil && httpRuleInfo.ID > 0 {
+		tx.Rollback()
+		middleware.ResponseError(c, 2003, errors.New("服务接入前缀或域名已存在"))
+		return
+	}
+
+	serviceModel := &dao.ServiceInfo{
+		ServiceName: params.ServiceName,
+		ServiceDesc: params.ServiceDesc,
+	}
+	if err := serviceModel.Save(c, tx); err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2005, err)
+		return
+	}
+	//serviceModel.ID
+	httpRule := &dao.HttpRule{
+		ServiceID:      serviceModel.ID,
+		RuleType:       params.RuleType,
+		Rule:           params.Rule,
+		NeedHttps:      params.NeedHttps,
+		NeedStripUri:   params.NeedStripUri,
+		NeedWebsocket:  params.NeedWebsocket,
+		UrlRewrite:     params.UrlRewrite,
+		HeaderTransfor: params.HeaderTransfor,
+	}
+	if err := httpRule.Save(c, tx); err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2006, err)
+		return
+	}
+
+	accessControl := &dao.AccessControl{
+		ServiceID:         serviceModel.ID,
+		OpenAuth:          params.OpenAuth,
+		BlackList:         params.BlackList,
+		WhiteList:         params.WhiteList,
+		ClientIPFlowLimit: params.ClientipFlowLimit,
+		ServiceFlowLimit:  params.ServiceFlowLimit,
+	}
+	if err := accessControl.Save(c, tx); err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2007, err)
+		return
+	}
+
+	loadbalance := &dao.LoadBalance{
+		ServiceID:              serviceModel.ID,
+		RoundType:              params.RoundType,
+		IpList:                 params.IpList,
+		WeightList:             params.WeightList,
+		UpstreamConnectTimeout: params.UpstreamConnectTimeout,
+		UpstreamHeaderTimeout:  params.UpstreamHeaderTimeout,
+		UpstreamIdleTimeout:    params.UpstreamIdleTimeout,
+		UpstreamMaxIdle:        params.UpstreamMaxIdle,
+	}
+	if err := loadbalance.Save(c, tx); err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2008, err)
+		return
+	}
+	tx.Commit()
+	middleware.ResponseSuccess(c, "")
+}
+
+func (service *ServiceController) ServiceUpdateHTTP(c *gin.Context) {
+	params := &dto.ServiceUpdateHTTPInput{}
+	if err := params.BindValidParam(c); err != nil {
+		middleware.ResponseError(c, 2000, err)
+		return
+	}
+
+	if len(strings.Split(params.IpList, ",")) != len(strings.Split(params.WeightList, ",")) {
+		middleware.ResponseError(c, 2001, errors.New("IP列表与权重列表数量不一致"))
+		return
+	}
+
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	tx = tx.Begin()
+	serviceInfo := &dao.ServiceInfo{ServiceName: params.ServiceName}
+	// 1. 执行查询
+	serviceInfo, err = serviceInfo.Find(c, tx, serviceInfo)
+	// 2. 先判断数据库查询是否出错（比如表不存在、连接失败）
+	if err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2003, err) // 这里返回真实错误，不是“服务不存在”
+		return
+	}
+	// 3. 关键判断：ID>0 才是真实存在的服务（核心修改）
+	if serviceInfo == nil || serviceInfo.ID <= 0 {
+		tx.Rollback()
+		middleware.ResponseError(c, 2003, errors.New("服务不存在"))
+		return
+	}
+
+	// 4. 查询服务详情（关联表数据）
+	serviceDetail, err := serviceInfo.ServiceDetail(c, tx, serviceInfo)
+	if err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2004, err) // 这里返回查询详情的错误，不是“服务不存在”
+		return
+	}
+	// 5. 额外校验：关联表数据是否存在（可选，但更严谨）
+	if serviceDetail.HTTPRule == nil || serviceDetail.HTTPRule.ID <= 0 {
+		tx.Rollback()
+		middleware.ResponseError(c, 2004, errors.New("服务HTTP规则不存在"))
+		return
+	}
+	if serviceDetail.LoadBalance == nil || serviceDetail.LoadBalance.ID <= 0 {
+		tx.Rollback()
+		middleware.ResponseError(c, 2004, errors.New("服务负载均衡规则不存在"))
+		return
+	}
+
+	// 后续更新逻辑不变...
+	info := serviceDetail.Info
+	info.ServiceDesc = params.ServiceDesc
+	if err := info.Save(c, tx); err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2005, err)
+		return
+	}
+
+	httpRule := serviceDetail.HTTPRule
+	httpRule.NeedHttps = params.NeedHttps
+	httpRule.NeedStripUri = params.NeedStripUri
+	httpRule.NeedWebsocket = params.NeedWebsocket
+	httpRule.UrlRewrite = params.UrlRewrite
+	httpRule.HeaderTransfor = params.HeaderTransfor
+	if err := httpRule.Save(c, tx); err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2006, err)
+		return
+	}
+
+	accessControl := serviceDetail.AccessControl
+	accessControl.OpenAuth = params.OpenAuth
+	accessControl.BlackList = params.BlackList
+	accessControl.WhiteList = params.WhiteList
+	accessControl.ClientIPFlowLimit = params.ClientipFlowLimit
+	accessControl.ServiceFlowLimit = params.ServiceFlowLimit
+	if err := accessControl.Save(c, tx); err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2007, err)
+		return
+	}
+
+	loadbalance := serviceDetail.LoadBalance
+	loadbalance.RoundType = params.RoundType
+	loadbalance.IpList = params.IpList
+	loadbalance.WeightList = params.WeightList
+	loadbalance.UpstreamConnectTimeout = params.UpstreamConnectTimeout
+	loadbalance.UpstreamHeaderTimeout = params.UpstreamHeaderTimeout
+	loadbalance.UpstreamIdleTimeout = params.UpstreamIdleTimeout
+	loadbalance.UpstreamMaxIdle = params.UpstreamMaxIdle
+	if err := loadbalance.Save(c, tx); err != nil {
+		tx.Rollback()
+		middleware.ResponseError(c, 2008, err)
+		return
+	}
+	tx.Commit()
+	middleware.ResponseSuccess(c, "")
 }
--- dao/service_info.go
+++ dao/service_info.go
@@ -14,11 +14,56 @@ type ServiceInfo struct {
 	ServiceDesc string    `json:"service_desc" gorm:"column:service_desc" description:"服务描述"`
 	UpdatedAt   time.Time `json:"create_at" gorm:"column:create_at" description:"更新时间"`
 	CreatedAt   time.Time `json:"update_at" gorm:"column:update_at" description:"添加时间"`
-	IsDelete    int8      `json:"is_delete" gorm:"column:is_delete" description:"是否已删除；0：否�?：是"`
+	IsDelete    int8      `json:"is_delete" gorm:"column:is_delete" description:"是否已删除；0：否；1：是"`
 }
 
 func (t *ServiceInfo) TableName() string {
 	return "gateway_service_info"
+}
+
+func (t *ServiceInfo) ServiceDetail(c *gin.Context, tx *gorm.DB, search *ServiceInfo) (*ServiceDetail, error) {
+	if search.ServiceName == "" {
+		info, err := t.Find(c, tx, search)
+		if err != nil {
+			return nil, err
+		}
+		search = info
+	}
+	httpRule := &HttpRule{ServiceID: search.ID}
+	httpRule, err := httpRule.Find(c, tx, httpRule)
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	tcpRule := &TcpRule{ServiceID: search.ID}
+	tcpRule, err = tcpRule.Find(c, tx, tcpRule)
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	grpcRule := &GrpcRule{ServiceID: search.ID}
+	grpcRule, err = grpcRule.Find(c, tx, grpcRule)
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	accessControl := &AccessControl{ServiceID: search.ID}
+	accessControl, err = accessControl.Find(c, tx, accessControl)
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	loadBalance := &LoadBalance{ServiceID: search.ID}
+	loadBalance, err = loadBalance.Find(c, tx, loadBalance)
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+
+	detail := &ServiceDetail{
+		Info:          search,
+		HTTPRule:      httpRule,
+		TCPRule:       tcpRule,
+		GRPCRule:      grpcRule,
+		LoadBalance:   loadBalance,
+		AccessControl: accessControl,
+	}
+	return detail, nil
 }
 
 func (t *ServiceInfo) PageList(c *gin.Context, tx *gorm.DB, param *dto.ServiceListInput) ([]ServiceInfo, int64, error) {
@@ -36,49 +81,6 @@ func (t *ServiceInfo) PageList(c *gin.Context, tx *gorm.DB, param *dto.ServiceLi
 	}
 	query.Limit(param.PageSize).Offset(offset).Count(&total)
 	return list, total, nil
-}
-
-func (t *ServiceInfo) ServiceDetail(c *gin.Context, tx *gorm.DB, search *ServiceInfo) (*ServiceDetail, error) {
-	var err error
-	httpRule := &HttpRule{ServiceID: search.ID}
-	httpRule, err = httpRule.Find(c, tx, httpRule)
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return nil, err
-	}
-
-	grpcRule := &GrpcRule{ServiceID: search.ID}
-	grpcRule, err = grpcRule.Find(c, tx, grpcRule)
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return nil, err
-	}
-
-	tcpRule := &TcpRule{ServiceID: search.ID}
-	tcpRule, err = tcpRule.Find(c, tx, tcpRule)
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return nil, err
-	}
-
-	loadBlance := &LoadBalance{ServiceID: search.ID}
-	loadBlance, err = loadBlance.Find(c, tx, loadBlance)
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return nil, err
-	}
-
-	accessControl := &AccessControl{ServiceID: search.ID}
-	accessControl, err = accessControl.Find(c, tx, accessControl)
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return nil, err
-	}
-
-	detail := &ServiceDetail{
-		Info:          search,
-		HTTPRule:      httpRule,
-		TCPRule:       tcpRule,
-		GRPCRule:      grpcRule,
-		LoadBalance:   loadBlance,
-		AccessControl: accessControl,
-	}
-	return detail, nil
 }
 
 func (t *ServiceInfo) Find(c *gin.Context, tx *gorm.DB, search *ServiceInfo) (*ServiceInfo, error) {
